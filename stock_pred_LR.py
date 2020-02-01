9fffb20eef96d256eb529f911d2cb5c2e66b980d
Archan Mehta <35597922+Archan2607@users.noreply.github.com>
2020-02-02 00:17:15 +0530

    Add files via upload

---
+++ stock_pred_LR.py
@@ -0,0 +1,73 @@
+#importing necessary libraries
+
+import quandl, math
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing, svm
+from sklearn.model_selection import  cross_validate, train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime
+quandl.ApiConfig.api_key = "e192xvjDy8KJSEY8kemT"
+#setting style for graphical representation
+style.use('ggplot')
+
+
+#Using Quandl website to import stock data
+df = quandl.get("WIKI/GOOGL")
+
+#Taking required columns
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+
+#Deriving Features from existing columns
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+#Taking required columns for predicting stuff
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+
+#pre-processing
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
+
+#Dividing train-test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#clf = LinearRegression(n_jobs=-1)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+plt.savefig('results.png')
+print(confidence)
